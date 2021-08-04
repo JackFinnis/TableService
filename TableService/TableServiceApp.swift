19b7b2f03b94437f30e61665f93929669bdf5a63
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-04 12:08:52 +0100

    Initial Commit

---
+++ TableService/TableServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableServiceApp.swift
+//  TableService
+//
+//  Created by William Finnis on 04/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct TableServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
