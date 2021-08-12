5962cfddc1fc5dabe7a48aef087a22ebe4600626
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-12 13:43:49 +0100

    Initial Commit

---
+++ TableService/Views/MenuView.swift
@@ -0,0 +1,14 @@
+//
+//  MenuView.swift
+//  MenuView
+//
+//  Created by William Finnis on 04/08/2021.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Menu View")
+    }
+}
